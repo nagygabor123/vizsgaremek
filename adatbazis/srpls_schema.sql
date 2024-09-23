665868d53add3eda58c91b89bbfd6e4f19f6e438
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2024-09-23 13:06:08 +0200

    Update srpls_schema.sql

--- adatbazis/srpls_schema.sql
+++ adatbazis/srpls_schema.sql
@@ -1,11 +1,11 @@
--- Lockers tábla létrehozása
+
 CREATE TABLE lockers (
     locker_id INT PRIMARY KEY AUTO_INCREMENT,
-    status ENUM('open', 'closed') NOT NULL,
+    status ENUM('nyitva', 'zarva') NOT NULL,
     can_be_opened BOOLEAN DEFAULT FALSE -- Jelzi, hogy nyitható-e a szekrény, tanári jóváhagyást igényel
 );
 
--- Students tábla létrehozása
+
 CREATE TABLE students (
     student_id VARCHAR(20) PRIMARY KEY,
     full_name VARCHAR(255) NOT NULL,
@@ -15,19 +15,19 @@ CREATE TABLE students (
     rfid_tag VARCHAR(50) UNIQUE NOT NULL
 );
 
--- Create Locker Access History Table
+
 CREATE TABLE locker_access_history (
     access_id INT PRIMARY KEY AUTO_INCREMENT,
     rfid_tag VARCHAR(50) NOT NULL,
     locker_id INT NOT NULL,
-    access_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Rögzíti, mikor nyitották ki a szekrényt
-    approved_by INT, -- Annak az adminnak az azonosítója, aki jóváhagyta a hozzáférést (NULL lehet, ha nem volt szükséges)
+    access_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Logolja, mikor nyitották ki a szekrényt
+    approved_by INT, -- Annak az azonosítója, aki jóváhagyta a hozzáférést (NULL, ha a rendszer a nap végén nyitódik ki)
     FOREIGN KEY (rfid_tag) REFERENCES students(rfid_tag),
     FOREIGN KEY (locker_id) REFERENCES lockers(locker_id),
     FOREIGN KEY (approved_by) REFERENCES admins(admin_id)
 );
 
--- Create Locker Relationships Table
+
 CREATE TABLE locker_relationships (
     relationship_id INT PRIMARY KEY AUTO_INCREMENT,
     rfid_tag VARCHAR(50) NOT NULL,
@@ -36,28 +36,28 @@ CREATE TABLE locker_relationships (
     FOREIGN KEY (locker_id) REFERENCES lockers(locker_id)
 );
 
--- Create Subjects Table
+
 CREATE TABLE subjects (
     subject_id INT PRIMARY KEY AUTO_INCREMENT,
     subject_name VARCHAR(255) NOT NULL,
     teacher_name VARCHAR(255) NOT NULL
 );
 
--- Create Timetables Table
+
 CREATE TABLE timetables (
     timetable_id INT PRIMARY KEY AUTO_INCREMENT,
     student_id VARCHAR(20) NOT NULL,
     subject_id INT NOT NULL,
-    day_of_week ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday') NOT NULL,
+    day_of_week ENUM('Hetfo', 'Kedd', 'Szerda', 'Csutortok', 'Pentek') NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
     FOREIGN KEY (student_id) REFERENCES students(student_id),
     FOREIGN KEY (subject_id) REFERENCES subjects(subject_id)
 );
 
--- Create Admins Table (for teachers/staff who can approve locker access)
+
 CREATE TABLE admins (
     admin_id INT PRIMARY KEY AUTO_INCREMENT,
     full_name VARCHAR(255) NOT NULL,
-    position VARCHAR(255) NOT NULL -- Pl. 'Tanár', 'Adminisztrátor'
+    position VARCHAR(255) NOT NULL -- Pl. Rendszergazda, Igazgatóheléyettes
 );
