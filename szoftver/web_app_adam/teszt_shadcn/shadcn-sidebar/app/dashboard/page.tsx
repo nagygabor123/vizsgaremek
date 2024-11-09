7e1ad3f90eae83b8adb97a3969a6234221388664
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2024-11-09 22:32:00 +0100

    teszt

--- szoftver/web_app_adam/teszt_shadcn/shadcn-sidebar/app/dashboard/page.tsx
+++ szoftver/web_app_adam/teszt_shadcn/shadcn-sidebar/app/dashboard/page.tsx
@@ -14,6 +14,9 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 
+
+
+
 export default function Page() {
   return (
     <SidebarProvider>
@@ -40,6 +43,8 @@ export default function Page() {
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+         
+         
             {/* <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" />
             <div className="aspect-video rounded-xl bg-muted/50" /> */}
