5a149c365bef898f54e70045866e46cd9a609a52
nagygabor123 <119402639+nagygabor123@users.noreply.github.com>
2024-11-26 12:26:06 +0100

    adatbazis kapcsolat javitasa

--- szoftver/web_app_gabor/crud_nextjs/lib/db.js
+++ szoftver/web_app_gabor/crud_nextjs/lib/db.js
@@ -4,6 +4,31 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Kapcsolat-pool létrehozása
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,    // Várakozzon, ha nincs elérhető kapcsolat
+  connectionLimit: 10,         // A maximális kapcsolatok száma
+  queueLimit: 0                // Nincs várakozási limit
+});
+
+export async function connectToDatabase() {
+  const connection = await pool.getConnection();
+  return connection;
+}
+
+
+
+
+/* régi
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export async function connectToDatabase() {
   const connection = await mysql.createConnection({
     host: process.env.DB_HOST,
@@ -12,4 +37,4 @@ export async function connectToDatabase() {
     database: process.env.DB_NAME,
   });
   return connection;
-}
+}*/
