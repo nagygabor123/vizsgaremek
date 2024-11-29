b37859eb282f48a0b66c6bcaf0bb2dd428aa7b78
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2024-11-29 10:15:26 +0100

    api

---
+++ szoftver/web_app_adam/teszt_nextjs_api/crud_nextjs/lib/db.js
@@ -0,0 +1,41 @@
+// lib/db.js
+/*
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Kapcsolat-pool létrehozása
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,    // Várakozzon, ha nincs elérhető kapcsolat
+  connectionLimit: 10,         // A maximális kapcsolatok száma
+  queueLimit: 0                // Nincs várakozási limit
+});
+
+export async function connectToDatabase() {
+  const connection = await pool.getConnection();
+  return connection;
+}*/
+
+
+
+
+
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export async function connectToDatabase() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  return connection;
+}
