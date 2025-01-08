7323a7f5bf55f0b0747d3501d58135313bb8bd06
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-01-08 14:49:32 +0100

    Create screen.ino

---
+++ szoftver/web_app_adam/arduino_kijelzo/screen.ino
@@ -0,0 +1,24 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_GC9A01A.h>
+#include <SPI.h>
+
+// Kijelző lábkiosztása
+#define TFT_CS   10
+#define TFT_DC    9
+#define TFT_RST   8
+
+// Kijelző példány
+Adafruit_GC9A01A tft = Adafruit_GC9A01A(TFT_CS, TFT_DC, TFT_RST);
+
+void setup() {
+  tft.begin();                 // Kijelző inicializálása
+  tft.setRotation(0);          // Forgatás
+  tft.fillScreen(GC9A01A_YELLOW); // Törlés feketére
+  
+  // Rajzolj egy piros kört
+  tft.fillCircle(120, 120, 100, GC9A01A_BLACK);
+}
+
+void loop() {
+  // További funkciók helye
+}
