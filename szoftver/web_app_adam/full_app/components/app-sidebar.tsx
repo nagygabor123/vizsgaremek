6d9a5714b29fcb02af99d0755aca204adb0ce970
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-01-17 10:27:11 +0100

    Update app-sidebar.tsx

--- szoftver/web_app_adam/full_app/components/app-sidebar.tsx
+++ szoftver/web_app_adam/full_app/components/app-sidebar.tsx
@@ -228,61 +228,69 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     <Sidebar variant="inset" className="border-r-0" {...props}>
       <SidebarHeader>
       <DropdownMenu>
-<DropdownMenuTrigger asChild>
-  <SidebarMenuButton
-    size="lg"
-    className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-  >
-  <div className="grid flex-1 text-left text-sm leading-tight">
-      <span className="truncate text-s">Kiskunfélegyházi Szent</span>
-        <span className="truncate text-s">Benedek PG Középiskola</span>
-      </div>
-  
-   
-    <Avatar className="h-7 w-7 rounded-full ">
-      <AvatarFallback className="rounded-lg bg-lime-300 ml-auto">VZ</AvatarFallback>
-    </Avatar>
-    <ChevronDown className="ml-auto size-4" />
-  </SidebarMenuButton>
-</DropdownMenuTrigger>
-<DropdownMenuContent>
-  <DropdownMenuLabel className="p-0 font-normal">
-    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-      <Avatar className="h-9 w-9 rounded-full">
-        <AvatarFallback className="rounded-lg bg-lime-300">VZ</AvatarFallback>
-      </Avatar>
-      <div className="grid flex-1 text-left text-sm leading-tight">
-      <span className="truncate font-semibold">Vincze Zsolt</span>
-        <span className="truncate text-xs">vincze.zsolt@szbi-pg.hu</span>
-      </div>
-    </div>
-  </DropdownMenuLabel>
-  <DropdownMenuSeparator />
-{/* <DropdownMenuGroup>
-    <DropdownMenuItem>
-    <LifeBuoy/> 
-      Súgóközpont
-    </DropdownMenuItem>
-    <DropdownMenuItem>
-    <MessageCircleWarning/> 
-      Probléma jelentése
-    </DropdownMenuItem>
-    <DropdownMenuItem>
-    <Settings /> 
-      Beállítások
-    </DropdownMenuItem>
-  </DropdownMenuGroup>*/}
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+            
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate text-xs">Kiskunfélegyházi Szent</span>
+                <span className="truncate text-xs">Benedek PG Középiskola</span>
+              </div>
+              <Avatar className="h-8 w-8 rounded-full ">
+                <AvatarFallback className="rounded-lg bg-blue-200">VZ</AvatarFallback>
+              </Avatar>
+              <ChevronDown className="ml-auto size-4" />
+            
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="start"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-9 w-9 rounded-lg">
+                  <AvatarFallback className="rounded-full bg-blue-200">VZ</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">Vincze Zsolt</span>
+                  <span className="truncate text-xs">vincze.zsolt@szbi-pg.hu</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+               
+              <Settings/>
+              <span>Beállítások</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+               
+              <LifeBuoy/>
+              <span>Súgóközpont</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+              
+              <MessageCircleWarning/>
+              <span>Probléma jelentése</span>
+              </DropdownMenuItem>
 
-
-  <DropdownMenuSeparator />
-  <DropdownMenuGroup>
-  <DropdownMenuItem>
-   <LogOut /> 
-    Kijelentkezés
-  </DropdownMenuItem>
-  </DropdownMenuGroup>
-</DropdownMenuContent>
-</DropdownMenu>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+            <LogOut/>
+            <span>Kijelentkezés</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
 
         <Separator/>
       </SidebarHeader>
@@ -342,7 +350,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
  
 
     <SidebarGroup>
-          <SidebarGroupLabel>Iskola</SidebarGroupLabel>
+          <SidebarGroupLabel>Adminisztráció</SidebarGroupLabel>
       <SidebarMenu>
 
 {/*
@@ -383,7 +391,19 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 </SidebarMenuButton>
 </SidebarMenuItem>
 
-<SidebarMenuItem>
+
+
+      </SidebarMenu>
+    </SidebarGroup>
+ 
+
+
+
+    <SidebarGroup>
+          <SidebarGroupLabel>Rendszer</SidebarGroupLabel>
+      <SidebarMenu>
+
+      <SidebarMenuItem>
 <SidebarMenuButton asChild>
   <Link href="">
 <SlidersHorizontal/>
@@ -401,50 +421,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   </Link>
 </SidebarMenuButton>
 </SidebarMenuItem>
-
-      </SidebarMenu>
-    </SidebarGroup>
- 
-
-
-
-    <SidebarGroup>
-          <SidebarGroupLabel>Rendszer</SidebarGroupLabel>
-      <SidebarMenu>
-
-
-      <SidebarMenuItem>
-<SidebarMenuButton asChild>
-  <Link href="">
-  <LifeBuoy/>
-    <span>Súgóközpont</span>
-
-  </Link>
-</SidebarMenuButton>
-</SidebarMenuItem>
-
-
-<SidebarMenuItem>
-<SidebarMenuButton asChild>
-  <Link href="">
-  <MessageCircleWarning/>
-  <span>Probléma jelentése</span>
-
-  </Link>
-</SidebarMenuButton>
-</SidebarMenuItem>
-
-
-<SidebarMenuItem>
-<SidebarMenuButton asChild>
-  <Link href="">
-  <Settings/>
-    <span>Beállítások</span>
-
-  </Link>
-</SidebarMenuButton>
-</SidebarMenuItem>
-
 
       </SidebarMenu>
     </SidebarGroup>
