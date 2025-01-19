9bfaa7d76105c4ae65a064f6a944799e3e53cc0d
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-01-19 16:34:16 +0100

    Update app-sidebar.tsx

--- szoftver/web_app_adam/full_app/components/app-sidebar.tsx
+++ szoftver/web_app_adam/full_app/components/app-sidebar.tsx
@@ -149,7 +149,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               </div>
            
               <Avatar className="h-9 w-9 rounded-full">
-                <AvatarFallback className="rounded-lg bg-emerald-200">VZ</AvatarFallback>
+                <AvatarFallback className="rounded-lg bg-sky-200">VZ</AvatarFallback>
               </Avatar>
               <ChevronDown className="ml-auto size-4" />
             
@@ -164,7 +164,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-9 w-9 rounded-full">
-                  <AvatarFallback className="rounded-lg bg-emerald-200">VZ</AvatarFallback>
+                  <AvatarFallback className="rounded-lg bg-sky-200">VZ</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">Vincze Zsolt</span>
