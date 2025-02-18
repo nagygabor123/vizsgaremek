f4ebf25a5ab34f02dede302e397ac4a23f4a7e76
nagygabor123 <119402639+nagygabor123@users.noreply.github.com>
2025-02-18 11:38:40 +0100

    Student api-ok pipa

--- szoftver/full_app/pages/api/students/update.js
+++ szoftver/full_app/pages/api/students/update.js
@@ -82,9 +82,7 @@
  *                   type: string
  *                   example: "Error updating student"
  */
-
 import { connectToDatabase } from '../../../lib/db';
-
 
 export default async function handler(req, res) {
   if (req.method === 'PUT') {
@@ -103,6 +101,8 @@ export default async function handler(req, res) {
       res.status(200).json({ message: 'Student updated' });
     } catch (error) {
       res.status(500).json({ message: 'Error updating student' });
+    } finally {
+      await db.end();  
     }
   } else {
     res.status(405).json({ message: 'Method Not Allowed' });
