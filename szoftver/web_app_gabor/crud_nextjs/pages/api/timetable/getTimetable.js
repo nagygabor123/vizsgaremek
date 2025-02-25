05a63d9f8ffa8e1c66f581f6f37fc8e8f9c779ae
nagygabor123 <119402639+nagygabor123@users.noreply.github.com>
2025-02-25 12:19:36 +0100

    orarend lekerdezes rendben

---
+++ szoftver/web_app_gabor/crud_nextjs/pages/api/timetable/getTimetable.js
@@ -0,0 +1,32 @@
+
+import { connectToDatabase } from '../../../lib/db';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const teacherName = 'BeMo'; 
+
+    let connection;
+
+    try {
+      connection = await connectToDatabase();
+
+      const [results] = await connection.execute(
+        `SELECT t.day_of_week, t.start_time, t.end_time, t.group AS group_name, a.short_name AS teacher_name FROM timetables t JOIN admins a ON t.admin_id = a.admin_id WHERE a.short_name LIKE ? ORDER BY FIELD(t.day_of_week, 'monday', 'tuesday', 'wednesday', 'thursday', 'friday');
+`,
+        [teacherName]
+      );
+
+      return res.status(200).json(results);
+
+    } catch (error) {
+      console.error('Database error:', error);
+      return res.status(500).json({ error: 'Database connection error' });
+    } finally {
+      if (connection) {
+        await connection.end();
+      }
+    }
+  } else {
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
