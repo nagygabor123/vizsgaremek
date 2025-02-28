2698bfcf0591c6f0dd689ab2b4cf864df60a0924
nagygabor123 <119402639+nagygabor123@users.noreply.github.com>
2025-02-28 09:47:57 +0100

    rendezgetes + diákfeltoltes csv-bol

--- szoftver/full_app/app/dashboard/configuration/page.tsx
+++ szoftver/full_app/app/dashboard/configuration/page.tsx
@@ -47,7 +47,7 @@ const Configuration = () => {
     formData.append('file', selectedFile);
 
     try {
-      const response = await fetch('http://localhost:3000/api/config/addStudent', {
+      const response = await fetch('http://localhost:3000/api/setup/studentsToDatabase', {
         method: 'POST',
         body: formData,
       });
