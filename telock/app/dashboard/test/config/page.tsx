01e8ed1e95f40dddee2c691fd04179edcb5c043b
Nagy Gábor 1 <nagy.gabor@diak.szbi-pg.hu>
2025-03-07 10:10:38 +0100

    Update page.tsx

--- telock/app/dashboard/test/config/page.tsx
+++ telock/app/dashboard/test/config/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 
 const Configuration = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
