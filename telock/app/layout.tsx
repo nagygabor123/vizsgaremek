190931b3f1152f972623e7015a2adc06e589727a
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-08 20:56:16 +0100

    Revert "Update layout.tsx"
    This reverts commit ce8f52416956f620ec5807f4ed0c08f3da72d059.

--- telock/app/layout.tsx
+++ telock/app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession();
-  return (
+  return 
     <ClientLayout>{children}</ClientLayout>
-  );
+  ;
 }
