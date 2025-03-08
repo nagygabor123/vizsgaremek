6a76cd7712c78a7393d6b40b1701fd0f1805d5b4
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-08 11:14:18 +0100

    Update page.tsx

--- telock/app/dashboard/school/timetables/page.tsx
+++ telock/app/dashboard/school/timetables/page.tsx
@@ -207,7 +207,7 @@ const Calendar: React.FC = () => {
   useEffect(() => {
     async function fetchSchedule() {
       try {
-        const response = await fetch(`http://localhost:3000/api/timetable/getTeacherTimetable?teacherName=${teacher}`);
+        const response = await fetch(`https://vizsgaremek-mocha.vercel.app/api/timetable/getTeacherTimetable?teacherName=${teacher}`);
         const data = await response.json();
         const formattedData = data.map((lesson: any) => ({
           day: lesson.day_of_week,
