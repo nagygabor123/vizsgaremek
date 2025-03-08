af9cfd08295a6ae93df0cef7f9d470981be3cf15
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-08 14:05:52 +0100

    Update route.ts

--- telock/app/api/auth/[...nextauth]/route.ts
+++ telock/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from 'next-auth';
-import { authOptions } from './auth.config';
+import { authOptions } from '../auth.config';
 
 const handler = NextAuth(authOptions);
 
