aa5119818008722445d79811ebf69fc279a66a8d
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-08 14:03:50 +0100

    teszt4

--- telock/app/api/auth/[...nextauth]/route.ts
+++ telock/app/api/auth/[...nextauth]/route.ts
@@ -1,44 +1,6 @@
-import NextAuth, { SessionStrategy } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { compare } from 'bcrypt';
-import { neon } from '@neondatabase/serverless';
-
-export const authOptions = {
-  session: {
-    strategy: 'jwt' as SessionStrategy, // Explicit típusmegadás
-  },
-  pages: {
-    signIn: '/login',
-  },
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        short_name: {},
-        password: {},
-      },
-      async authorize(credentials, req) {
-        const sql = neon(`${process.env.DATABASE_URL}`);
-        const response = await sql`
-          SELECT * FROM admins WHERE short_name=${credentials?.short_name}`;
-        const user = response[0];
-
-        const passwordCorrect = await compare(
-          credentials?.password || '',
-          user.password
-        );
-
-        if (passwordCorrect) {
-          return {
-            id: user.id,
-            short_name: user.short_name,
-          };
-        }
-
-        return null;
-      },
-    }),
-  ],
-};
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from 'next-auth';
+import { authOptions } from './auth.config';
 
 const handler = NextAuth(authOptions);
 
