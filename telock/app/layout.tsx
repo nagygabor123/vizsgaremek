1c526515379d35d0685a7ba33e04adcc69f87e60
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-08 16:51:02 +0100

    Revert "login újból"
    This reverts commit fcb7b18d446e2f6927a6d4bcfa41ad9a8d0fade9.

--- telock/app/layout.tsx
+++ telock/app/layout.tsx
@@ -20,35 +20,25 @@ export const metadata: Metadata = {
   description: "Mobiltelefon tároló rendszer vezérlőpult",
 };
 
-interface User {
-  short_name?: string;  // Define the structure of user with optional short_name
-}
-
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const session = await getServerSession();
-  
-  // TypeScript type assertion to ensure that session and session.user are defined
-  const user = session?.user as User | undefined;
-
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>
-          {!!session ? (
-            <>
-              {/* Check if user exists and then display short_name */}
-              {user?.short_name && <span>Welcome, {user.short_name}</span>}
-              <Logout />
-            </>
-          ) : (
-            <Link href="/login">Login</Link>
-          )}
+           <nav>
+          {!!session && 
+          <Logout/>
+          }
+       
+          {!session && 
+          <Link href="/login">Login</Link>
+          }
         </nav>
         {children}
       </body>
