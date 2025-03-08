4a88eca0a18cc31e4e806043b8d7df8af0d09486
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-08 14:28:57 +0100

    Update app-sidebar.tsx

--- telock/components/app-sidebar.tsx
+++ telock/components/app-sidebar.tsx
@@ -50,7 +50,7 @@ import { useState, useEffect } from "react";
 import { signOut } from "next-auth/react";
 
 
-export async function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
    const { data: session } = useSession();
   const pathname = usePathname();
 
