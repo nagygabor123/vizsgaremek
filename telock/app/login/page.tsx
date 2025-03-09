c03a2d44663bb69e5952488c37480aaff648aabb
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-09 13:51:05 +0100

    Update page.tsx

--- telock/app/login/page.tsx
+++ telock/app/login/page.tsx
@@ -8,7 +8,7 @@ export default async function LoginPage(){
 
     const session = await getServerSession();
     if(session){
-      redirect("/dashboard/school/logs")
+      redirect("/dashboard")
     }
     return(
       <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
