2bb8f2114f93f9e4d7f17edef7bedfb35e4cbb8c
szalkaiszaboadam <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-09 09:13:58 +0100

    login page új

--- telock/app/login/page.tsx
+++ telock/app/login/page.tsx
@@ -11,6 +11,10 @@ export default async function LoginPage(){
       redirect("/")
     }
     return(
-      <Form/>
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <Form />
+      </div>
+    </div>
     )
 }
