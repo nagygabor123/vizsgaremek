9339a6068745313da52ff82d08b4f0414cb0462e
Szalkai-Szabó Ádám <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-11 11:22:03 +0100

    teszt log

--- telock/app/login/page.tsx
+++ telock/app/login/page.tsx
@@ -2,6 +2,13 @@
 import { getServerSession } from "next-auth";
 import Form from "./form";
 import { redirect } from "next/navigation";
+import localFont from "next/font/local";
+
+const ZenDots = localFont({
+  src: "./fonts/ZenDots-Regular.ttf",
+  variable: "--font-zen-dots",
+  weight: "100 900",
+});
 
 
 export default async function LoginPage(){
@@ -11,10 +18,18 @@ export default async function LoginPage(){
       redirect("/dashboard")
     }
     return(
-      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <Form />
-      </div>
-    </div>
+<div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+<div className="flex w-full max-w-sm flex-col gap-6">
+  <a href="#" className="flex items-center gap-2 self-center font-medium">
+   {/* <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+      <GalleryVerticalEnd className="size-4" />
+    </div>*/}
+    <span className={`${ZenDots.className} text-xl`}>
+                telock
+              </span>
+  </a>
+  <Form />
+</div>
+</div>
     )
 }
