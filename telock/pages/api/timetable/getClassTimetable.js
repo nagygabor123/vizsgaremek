9e14b3a8f6a2a9f006fb0c80fd24fc1b94f22af5
nagygabor123 <nagy.gabor@diak.szbi-pg.hu>
2025-03-14 21:20:14 +0100

    Update getClassTimetable.js

--- telock/pages/api/timetable/getClassTimetable.js
+++ telock/pages/api/timetable/getClassTimetable.js
@@ -12,37 +12,36 @@ export default async function handler(req, res) {
 
       const [results] = await sql(
         `SELECT 
-          t.day_of_week,
-          t.start_time,
-          t.end_time,
-          STRING_AGG(c.group_name, ', ' ORDER BY c.group_name) AS classes,
-          t.group_name AS group_name,
-          a.short_name AS teacher_name
+            t.day_of_week,
+            t.start_time,
+            t.end_time,
+            c.group_name AS class,
+            t.group_name AS group_name,
+            a.short_name AS teacher_name
         FROM 
-          timetables t
+            timetables t
         JOIN 
-          group_relations gr ON t.timetable_id = gr.timetable_id
+            group_relations gr ON t.timetable_id = gr.timetable_id
         JOIN 
-          csoportok c ON gr.group_id = c.group_id  
+            csoportok c ON gr.group_id = c.group_id  
         JOIN 
-          admins a ON t.admin_id = a.admin_id
+            admins a ON t.admin_id = a.admin_id
         WHERE 
-          c.group_name LIKE $1  
-        GROUP BY 
-          t.day_of_week, t.start_time, t.end_time, t.group_name, a.short_name
+            c.group_name LIKE $1  
         ORDER BY 
-          CASE t.day_of_week
-              WHEN 'monday' THEN 1
-              WHEN 'tuesday' THEN 2
-              WHEN 'wednesday' THEN 3
-              WHEN 'thursday' THEN 4
-              WHEN 'friday' THEN 5
-              WHEN 'saturday' THEN 6
-              WHEN 'sunday' THEN 7
-          END,
-          t.start_time;`,
+            CASE t.day_of_week
+                WHEN 'monday' THEN 1
+                WHEN 'tuesday' THEN 2
+                WHEN 'wednesday' THEN 3
+                WHEN 'thursday' THEN 4
+                WHEN 'friday' THEN 5
+                WHEN 'saturday' THEN 6
+                WHEN 'sunday' THEN 7
+            END,
+            t.start_time;`,
         [`${className}%`] 
       );
+      
       
 
       return res.status(200).json(results);
