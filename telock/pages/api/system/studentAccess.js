8ede9350bd332a15f1620aa3affa70af7f5072ad
nagygabor123 <nagy.gabor@diak.szbi-pg.hu>
2025-03-15 11:10:06 +0100

    Update studentAccess.js

--- telock/pages/api/system/studentAccess.js
+++ telock/pages/api/system/studentAccess.js
@@ -1,34 +1,7 @@
-/**
- * @swagger
- * /api/system/studentAccess:
- *   post:
- *     summary: Diák hozzáférésének ideiglenes megnyitása
- *     description: A megadott diák "access" állapotát "nyithato" értékre állítja, majd 10 perc elteltével automatikusan "zarva" állapotra váltja vissza.
- *     parameters:
- *       - in: query
- *         name: student
- *         required: true
- *         description: A diák azonosítója.
- *         schema:
- *           type: string
- *           enum: [OM11111, OM22222, OM33333, OM44444]
- *     tags:
- *       - System
- *     responses:
- *       200:
- *         description: Sikeres állapotfrissítés és időzített esemény létrehozása.
- *       400:
- *         description: Hiányzó diák azonosító.
- *       404:
- *         description: A megadott diák nem található.
- *       500:
- *         description: Hiba történt az adatbázis frissítése vagy az esemény létrehozása közben.
- */
-
 import { neon } from '@neondatabase/serverless';
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method !== 'POST') {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
