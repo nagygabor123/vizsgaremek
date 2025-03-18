569e116fbc5351a417a5fcec16d51bfb8e43dec4
nagygaga <nagy.gabor@diak.szbi-pg.hu>
2025-03-18 14:31:18 +0100

    Update getLocker.js

--- telock/pages/api/locker/getLocker.js
+++ telock/pages/api/locker/getLocker.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
 
       const schedule = await scheduleResponse.json();
       const { first_class_start, last_class_end } = schedule[0] || {};
-      const currentTime = new Date(aktido).toLocaleTimeString('en-US', { hour12: false, timeZone: 'Europe/Budapest', hour: '2-digit', minute: '2-digit' });
+      const currentTime = aktido;
       console.log(schedule);
       console.log(`Aktuális idő: ${currentTime}`);
       console.log(`Első óra kezdete: ${schedule.first_class_start}`);
