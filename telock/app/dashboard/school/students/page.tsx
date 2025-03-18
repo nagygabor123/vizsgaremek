e7cd38feefb1bf82779af64c6f8337683769e824
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2025-03-18 19:48:20 +0100

    7

--- telock/app/dashboard/school/students/page.tsx
+++ telock/app/dashboard/school/students/page.tsx
@@ -376,8 +376,8 @@ export default function Home() {
         <div>
     {loading ? (
         <div className="flex items-center justify-center min-h-screen">
-        <div className="animate-spin rounded-full h-8 w-8 border border-gray-300 border-t-blue-500"></div>
-      </div>
+        <div className="animate-spin rounded-full h-6 w-6 border-2 border-gray-300 border-t-blue-500"></div>
+       </div>
     ) : (
       <>
         {!hasStudents && <AppKonfig />}
