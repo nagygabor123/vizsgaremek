764fb2fa3a4d0f521024d920bed1ee11efe5751f
nagygabor123 <nagy.gabor@diak.szbi-pg.hu>
2025-03-20 15:40:02 +0100

    Update setLockerStatus.js

--- telock/pages/api/locker/setLockerStatus.js
+++ telock/pages/api/locker/setLockerStatus.js
@@ -1,17 +1,18 @@
-
 import { neon } from '@neondatabase/serverless';
 
 const sql = neon(process.env.DATABASE_URL);
+
 export default async function handler(req, res) {
   if (req.method === 'PATCH') {
     const { id } = req.query;
     const lockerId = parseInt(id);
+
     if (isNaN(lockerId) || lockerId < 1 || lockerId > 99) {
       return res.status(400).json({ message: 'Érvénytelen szekrény Id' });
     }
 
     try {
-      const [rows] = await sql('SELECT status FROM lockers WHERE locker_id = $1', [lockerId]);
+      const { rows } = await sql('SELECT status FROM lockers WHERE locker_id = $1', [lockerId]);
 
       if (rows.length === 0) {
         return res.status(404).json({ message: 'Szekrény nem található' });
@@ -19,20 +20,21 @@ export default async function handler(req, res) {
 
       const currentStatus = rows[0].status;
       const newStatus = currentStatus === 'be' ? 'ki' : 'be';
-      const [result] = await sql(
+
+      const { rowCount } = await sql(
         'UPDATE lockers SET status = $1 WHERE locker_id = $2',
         [newStatus, lockerId]
       );
 
-      if (result.affectedRows === 0) {
-        return res.status(500).json({ message: 'Sikertelen szekrény státusz frissités' });
+      if (rowCount === 0) {
+        return res.status(500).json({ message: 'Sikertelen szekrény státusz frissítés' });
       }
 
-      res.status(200).json({ message: `Szekrény ${lockerId} státusza frissítve '${newStatus}' (-re)` });
+      res.status(200).json({ message: `Szekrény ${lockerId} státusza frissítve '${newStatus}'-re` });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Szerver hiba' });
-    } 
+    }
   } else {
     res.status(405).json({ message: 'A módszer nem engedélyezett' });
   }
