ecb376eff4ae81d51721f75da27cf17916a61328
nagygabor123 <nagy.gabor@diak.szbi-pg.hu>
2025-03-20 17:00:31 +0100

    Update update.js

--- telock/pages/api/students/update.js
+++ telock/pages/api/students/update.js
@@ -142,8 +142,7 @@ export default async function handler(req, res) {
       );
 
       await deleteStudent(student_id);
-      const result = await setStudentGroups(newStudentId);
-      console.log('Student groups: ', result);
+      await setStudentGroups(newStudentId);
       return res.status(200).json({ message: 'Student and locker relationship updated successfully' });
     }
 
