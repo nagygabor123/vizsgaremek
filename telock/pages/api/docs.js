3d094ed10c0b748ed3e56c8a0a2c9e8a39d2c156
nagygaga <nagy.gabor@diak.szbi-pg.hu>
2025-03-21 10:27:51 +0100

    j

--- telock/pages/api/docs.js
+++ telock/pages/api/docs.js
@@ -3,6 +3,7 @@ import swaggerSpec from '../../swagger';
 export default function handler(req, res) {
   if (req.method === 'GET') {
     res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Engedélyezze a CORS-t
     res.status(200).json(swaggerSpec);
   } else {
     res.setHeader('Allow', ['GET']);
