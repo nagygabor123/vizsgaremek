3254b41cbf4a1e3567097dd2150d4dca06b101b7
nagygaga <nagy.gabor@diak.szbi-pg.hu>
2025-03-21 07:59:22 +0100

    Update getLocker.js

--- telock/pages/api/locker/getLocker.js
+++ telock/pages/api/locker/getLocker.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
 
       console.log(studentid); 
       console.log(studentaccess); 
-      console.log(scheduleResponse); 
+      console.log(schedule); 
       console.log(currentTime); 
 
 
