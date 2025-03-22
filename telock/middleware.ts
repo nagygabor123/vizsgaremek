7711b2ede167ed9b14a1207ebd3243d7e47a62ca
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2025-03-22 09:56:56 +0100

    Update middleware.ts

--- telock/middleware.ts
+++ telock/middleware.ts
@@ -6,24 +6,20 @@ export default withAuth(
   function middleware(req) {
     const token = req.nextauth.token;
 
-    if (req.nextUrl.pathname.startsWith("/dashboard/school")) {
+    if (req.nextUrl.pathname === "/dashboard/school/students") {
+      // Csak a "portas" pozícióval rendelkező felhasználók férhetnek hozzá
+      if (token?.position !== "portas") {
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+      }
+    } else if (req.nextUrl.pathname.startsWith("/dashboard/school")) {
+      // Csak az "igazgato", "igazgatohelyettes" és "rendszergazda" pozícióval rendelkező felhasználók férhetnek hozzá
       const allowedPositions = ["igazgato", "igazgatohelyettes", "rendszergazda"];
       if (typeof token?.position === "string" && !allowedPositions.includes(token.position)) {
         return NextResponse.redirect(new URL("/dashboard", req.url));
       }
-    }
-
-   
-    if (req.nextUrl.pathname.startsWith("/dashboard/school/students")) {
-      if (token?.position !== "portas" ) {
-        return NextResponse.redirect(new URL("/dashboard", req.url));
-      }
-    }
-
-
-    // Példa: Csak az osztályfőnökök férhetnek hozzá a /dashboard/class route-hoz
-    if (req.nextUrl.pathname.startsWith("/dashboard/class")) {
-      if (token?.osztalyfonok == "nincs") {
+    } else if (req.nextUrl.pathname.startsWith("/dashboard/class")) {
+      // Csak akkor engedélyezett a hozzáférés, ha az "osztalyfonok" nem "nincs"
+      if (token?.osztalyfonok === "nincs") {
         return NextResponse.redirect(new URL("/dashboard", req.url));
       }
     }
