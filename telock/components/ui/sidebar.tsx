f97efdd148762e38c0a10bae61b2fd8a8396ce8e
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2025-03-23 09:40:24 +0100

    button

--- telock/components/ui/sidebar.tsx
+++ telock/components/ui/sidebar.tsx
@@ -471,14 +471,14 @@ function SidebarMenuItem({ className, ...props }: React.ComponentProps<"li">) {
 }
 
 const sidebarMenuButtonVariants = cva(
-  "peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-blue-600 focus-visible:ring-2 active:bg-sidebar-accent active:text-blue-600 disabled:pointer-events-none disabled:opacity-50 group-has-data-[sidebar=menu-action]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-blue-600 data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-blue-600 group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
+  "peer/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-hidden ring-sidebar-ring transition-[width,height,padding] hover:bg-sidebar-accent hover:text-blue-600 focus-visible:ring-2 active:bg-sidebar-accent disabled:pointer-events-none disabled:opacity-50 group-has-data-[sidebar=menu-action]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:bg-sidebar-accent data-[active=true]:font-medium data-[active=true]:text-blue-600 data-[active=true]:border data-[active=true]:border-blue-600 data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-blue-600 group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
   {
     variants: {
       variant: {
         default:
-          "hover:bg-sidebar-accent hover:text-blue-600 hover:border hover:border-blue-600 active:border active:border-blue-600",
+          "hover:bg-sidebar-accent hover:text-blue-600 hover:border hover:border-blue-600", // Hover állapotban keret
         outline:
-          "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-blue-600 hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))] active:border active:border-blue-600",
+          "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-blue-600 hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]", // Hover állapotban keret
       },
       size: {
         default: "h-8 text-sm",
