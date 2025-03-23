ca29cf5a626160f13bc6add1a99dc770efa8c66f
nagygabor123 <nagy.gabor@diak.szbi-pg.hu>
2025-03-23 19:57:13 +0100

    l

--- telock/pages/api/config/getRinging.js
+++ telock/pages/api/config/getRinging.js
@@ -2,7 +2,7 @@ import { neon } from '@neondatabase/serverless';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Csak a GET metódus használható' });
+    return res.status(405).json({ error: 'A HTTP metódus nem engedélyezett' });
   }
 
   try {
