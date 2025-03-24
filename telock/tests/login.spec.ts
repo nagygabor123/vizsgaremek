79f95ce47f662778f59d3cead755b204b4abf368
Szalkai-Szabó Ádám <119402616+szalkaiszaboadam@users.noreply.github.com>
2025-03-24 09:54:50 +0100

    frontend teszt és table

---
+++ telock/tests/login.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Bejelentkezés teszt", () => {
+  test("Sikeres bejelentkezés", async ({ page }) => {
+    await page.goto("/login");
+
+    // Adatok kitöltése
+    await page.fill('input[name="short_name"]', "AdAd"); // Használj valós felhasználót
+    await page.fill('input[name="password"]', "admin");
+    await page.click('button[type="submit"]');
+
+    // Várakozás, hogy a sikeres bejelentkezés üzenet megjelenjen
+    const successAlert = page.locator('div[role="alert"]:has-text("Sikeres bejelentkezés")');
+
+    // Ellenőrizd, hogy az üzenet látható
+    await expect(successAlert).toBeVisible();
+  });
+
+  test("Sikertelen bejelentkezés", async ({ page }) => {
+    await page.goto("/login");
+
+    // Hibás adatokat adunk meg
+    await page.fill('input[name="short_name"]', "admin");
+    await page.fill('input[name="password"]', "rossz_jelszó");
+    await page.click('button[type="submit"]');
+
+    // Ellenőrizd, hogy megjelenik a hibaüzenet
+    await expect(page.locator("text=Hibás felhasználónév vagy jelszó!")).toBeVisible();
+  });
+});
