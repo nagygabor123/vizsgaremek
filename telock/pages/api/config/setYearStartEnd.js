25ac87e76959fdd73a84d052e935d535cb0da201
nagygaga <nagy.gabor@diak.szbi-pg.hu>
2025-03-28 09:49:32 +0100

    Update setYearStartEnd.js

--- telock/pages/api/config/setYearStartEnd.js
+++ telock/pages/api/config/setYearStartEnd.js
@@ -17,12 +17,7 @@ export default async function handler(req, res) {
       const query = 'UPDATE year_schedule SET which_day = $1 WHERE type = $2';
       const values = [which_day, type];
       const result = await sql(query, values);
-
-      if (result.length > 0) {
-        return res.status(200).json({ message: 'Sikeres frissítés', updatedType: type, updatedDate: which_day });
-      } else {
-        return res.status(404).json({ error: 'Nem található a megfelelő rekord' });
-      }
+      return res.status(200).json({ message: 'Sikeres frissítés', updatedType: type, updatedDate: which_day });
     } catch (error) {
       console.error('Hiba a frissítés során:', error);
       return res.status(500).json({ error: 'Hiba a frissítés során' });
