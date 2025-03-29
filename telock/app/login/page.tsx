56563dde15bc3c0fe013a4bd1b4e9cf4a04d2fe5
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2025-03-29 12:30:16 +0100

    3

--- telock/app/login/page.tsx
+++ telock/app/login/page.tsx
@@ -1,25 +1,6 @@
-'use client'
-
-import { FormEvent, useState } from "react";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button"
-import { ChevronLeft, AlertCircle, CircleCheck } from "lucide-react";
-import Link from 'next/link';
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 import { getServerSession } from "next-auth";
-// import Form from "./form";
+import Form from "./form";
 import { redirect } from "next/navigation";
 import localFont from "next/font/local";
 
@@ -30,32 +11,6 @@ const ZenDots = localFont({
 });
 
 export default async function LoginPage() {
-  const router = useRouter();
-  const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState<boolean>(false);
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setError(null);
-    setSuccess(false);
-
-    const formData = new FormData(e.currentTarget);
-    const response = await signIn('credentials', {
-      short_name: formData.get('short_name') as string,
-      password: formData.get('password') as string,
-      redirect: false,
-    });
-
-    if (response?.error) {
-      setError("Hibás felhasználónév vagy jelszó!");
-    } else {
-      setSuccess(true);
-      setTimeout(() => {
-        router.push("/dashboard");
-        router.refresh();
-      }, 1500);
-    }
-  };
 
   const session = await getServerSession();
   if (session) {
@@ -69,81 +24,7 @@ export default async function LoginPage() {
             telock
           </span>
         </a>
-        <div className="flex flex-col gap-6">
-      <Button variant="ghost" className="absolute left-4 top-4 p-2" asChild>
-        <Link href="/">
-          <ChevronLeft /> Vissza
-        </Link>
-      </Button>
-      <Card>
-        <CardHeader className="text-center">
-          <CardTitle className="text-xl">Bejelentkezés</CardTitle>
-          <CardDescription>
-            Üdvözöljük! Kérjük, adja meg bejelentkezési adatait a folytatáshoz.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit}>
-            <div className="grid gap-6">
-              <div className="grid gap-6">
-                <div className="grid gap-2">
-                  <Label htmlFor="short_name">Felhasználónév</Label>
-                  <Input
-                    name="short_name"
-                    type="text"
-                    required
-                  />
-                </div>
-                <div className="grid gap-2">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Jelszó</Label>
-                    {/* <a
-                      href="#"
-                      className="ml-auto text-sm underline-offset-4 hover:underline"
-                    >
-                      Elfelejtette jelszavát? 
-                    </a>*/}
-                  </div>
-                  <Input name="password" type="password" required />
-                </div>
-                <Button type="submit" className="w-full">
-                  Bejelentkezés
-                </Button>
-
-                {error &&
-                  <Alert variant="destructive">
-                    <AlertCircle className="h-4 w-4" />
-                    <AlertTitle>Sikeretlen bejelentkezés</AlertTitle>
-                    <AlertDescription>
-                      {error}
-                    </AlertDescription>
-                  </Alert>}
-                {success &&
-
-                  <Alert variant="siker">
-                    <CircleCheck className="h-4 w-4" />
-                    <AlertTitle>Sikeres bejelentkezés</AlertTitle>
-                    <AlertDescription>
-                      Nem sokára átirányítunk a vezérlőpultra.
-                    </AlertDescription>
-                  </Alert>
-
-                }
-              </div>
-              {/* <div className="text-center text-sm">
-              Szeretné használni a rendszerünket?{" "} 
-                <a href="#" className="underline underline-offset-4">
-                Lépjen velünk kapcsolatba
-                </a>
-              </div> */}
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-        Az adatait biztonságban tartjuk. Soha ne ossza meg jelszavát másokkal!
-      </div>
-    </div>
+        <Form />
       </div>
     </div>
   )
