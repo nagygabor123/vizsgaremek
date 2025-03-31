837498295e6dcb8db4826640d097886682ce2936
nagygabor123 <119402639+nagygabor123@users.noreply.github.com>
2025-03-31 10:40:20 +0200

    Update read.js

--- telock/pages/api/students/read.js
+++ telock/pages/api/students/read.js
@@ -5,20 +5,28 @@ export default async function handler(req, res) {
     const { school_id } = req.query;
     console.log(school_id);
     const sql = neon(process.env.DATABASE_URL);
+
     try {
-      const students = await sql(
-        `SELECT 
+      const query = `
+        SELECT 
           students.*, 
           lockers.status AS status
         FROM 
           students
-        WHERE school_id = ${school_id}
         LEFT JOIN 
           locker_relationships ON students.rfid_tag = locker_relationships.rfid_tag
         LEFT JOIN 
-          lockers ON locker_relationships.locker_id = lockers.locker_id`
-      );
-      
+          lockers ON locker_relationships.locker_id = lockers.locker_id
+        WHERE 
+          students.school_id = $1
+      `;
+
+      const students = await sql(query, [school_id]);
+
+      if (students.length === 0) {
+        return res.status(404).json({ message: 'Nincs találat' });
+      }
+
       res.status(200).json(students);
     } catch (error) {
       console.error('Adatbázis hiba:', error);
