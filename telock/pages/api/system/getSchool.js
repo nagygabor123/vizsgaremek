767e6c71be13436ccb3a7d11f36b463b5d3f69a3
nagygaga <nagy.gabor@diak.szbi-pg.hu>
2025-03-31 07:52:19 +0200

    Update getSchool.js

--- telock/pages/api/system/getSchool.js
+++ telock/pages/api/system/getSchool.js
@@ -1,33 +1,29 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.DATABASE_URL);
-
 export default async function handler(req, res) {
-  if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'A HTTP metódus nem engedélyezett' });
-  }
+  if (req.method === 'GET') {
+    const { school_id } = req.query;
 
-  const { school_id } = req.query;
-
-  if (!school_id) {
-    return res.status(400).json({ error: 'Iskola aznosító szükséges' });
-  }
-
-  const id = parseInt(school_id, 10);
-  if (isNaN(id)) {
-    return res.status(400).json({ error: 'Érvénytelen iskola azonosító' });
-  }
-
-  try {
-    const rows = await sql(`SELECT school_name FROM students WHERE school_id = ${id}`);
-
-    if (rows.length > 0) {
-      return res.status(200).json({ message: 'Sikeres lekérdezés', data: rows });
-    } else {
-      return res.status(404).json({ error: 'Iskola nem található' });
+    if (!school_id) {
+      return res.status(400).json({ error: 'Iskola aznosító szükséges' });
     }
-  } catch (error) {
-    console.error('Adatbázis hiba:', error);
-    return res.status(500).json({ error: 'Hiba az adatok lekérdezésekor', details: error.message });
+
+    const sql = neon(process.env.DATABASE_URL);
+
+    try {
+      const rows = await sql('SELECT school_name FROM schools WHERE school_id = $1', [school_id]);
+
+      if (rows.length > 0) {
+       // const access = rows[0].access;
+        return res.status(200).json({ message: 'Sikeres lekérdezés', data: rows });
+      } else {
+        return res.status(404).json({ error: 'Iskola nem található' });
+      }
+    } catch (error) {
+      console.error('Adatbazis hiba:', error);
+      return res.status(500).json({ error: 'Hiba az adatok lekérdezésekor' });
+    } 
+  } else {
+    return res.status(405).json({ error: 'A HTTP metódus nem engedélyezett' });
   }
 }
