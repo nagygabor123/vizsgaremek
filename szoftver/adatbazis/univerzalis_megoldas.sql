3131278142c3d27c944e7149c603efc2cb013db5
nagygabor123 <119402639+nagygabor123@users.noreply.github.com>
2025-03-31 10:32:06 +0200

    Update univerzalis_megoldas.sql

--- szoftver/adatbazis/univerzalis_megoldas.sql
+++ szoftver/adatbazis/univerzalis_megoldas.sql
@@ -1,6 +1,7 @@
 CREATE TABLE schools (
   school_id SERIAL PRIMARY KEY,
-  school_name VARCHAR(255) NOT NULL UNIQUE
+  school_name VARCHAR(255) NOT NULL UNIQUE,
+  status VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE admins (
@@ -72,13 +73,6 @@ CREATE TABLE group_relations (
   FOREIGN KEY (group_id) REFERENCES csoportok (group_id) ON DELETE CASCADE
 );
 
-CREATE TABLE system_status (
-  id SERIAL PRIMARY KEY,
-  status VARCHAR(10) NOT NULL,
-  school_id INT NOT NULL,
-  FOREIGN KEY (school_id) REFERENCES schools (school_id) ON DELETE CASCADE
-);
-
 CREATE TABLE year_schedule (
   year_schedule_id SERIAL PRIMARY KEY,
   type VARCHAR(255) NOT NULL,
@@ -97,14 +91,11 @@ CREATE TABLE ring_times (
   FOREIGN KEY (school_id) REFERENCES schools (school_id) ON DELETE CASCADE
 );
 
-INSERT INTO schools (school_name) VALUES
-('Kiskunfélegyházi Szent Benedek PG Középiskola');
+INSERT INTO schools (school_name,status) VALUES
+('Kiskunfélegyházi Szent Benedek PG Középiskola', 'nyithato');
 
 INSERT INTO admins (full_name, password, position, osztalyfonok, short_name, school_id) VALUES
 ('Admin Admin', '$2b$10$O5qBB4FhkxxTT9LBvuopzuA9y2L5.qrQxYjLOsLyYY8ofYCCJW1HW', 'rendszergazda', 'nincs', 'AdAd', 1);
-
-INSERT INTO system_status (status, school_id) VALUES
-('nyithato', 1);
 
 INSERT INTO year_schedule (type, nev, which_day, replace_day, school_id) VALUES
 ('kezd', 'Tanévkezdés', '2024-09-02', '', 1),
