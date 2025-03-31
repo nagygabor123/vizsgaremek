ed70111b18090081deffe5f871c0f0fc718fa88b
nagygabor123 <119402639+nagygabor123@users.noreply.github.com>
2025-03-31 11:23:58 +0200

    Update closeOpen.js

--- telock/pages/api/system/closeOpen.js
+++ telock/pages/api/system/closeOpen.js
@@ -12,8 +12,8 @@ export default async function handler(req, res) {
     const newAccessState = action === 'close' ? 'zarva' : 'nyithato';
 
     try {
-      await sql('UPDATE students SET access = $1 WHERE school_id = $2', [newAccessState],[school_id]);
-      await sql('UPDATE schools SET status = $1 WHERE school_id = $2', [newAccessState],[school_id]);
+      await sql('UPDATE students SET access = $1 WHERE school_id = $2', [newAccessState, school_id]);
+      await sql('UPDATE schools SET status = $1 WHERE school_id = $2', [newAccessState, school_id]);
 
       return res.status(200).json({ message: `Az összes diák 'access' mezője és a 'status' ${newAccessState} (-ra) frissítve` });
     } catch (error) {
