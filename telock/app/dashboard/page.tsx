1dff920e60a9ed9de69d881b99cc7aeb156d8491
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2025-04-02 14:44:21 +0200

    Update page.tsx

--- telock/app/dashboard/page.tsx
+++ telock/app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { AppSidebar } from "@/components/app-sidebar"
 import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react";
 
 import {
   Breadcrumb,
@@ -22,10 +23,35 @@ import { Megaphone } from "lucide-react";
 export default function Page() {
   const { data: session } = useSession();
 
+  const [students, setStudents] = useState([]);
+  const [hasStudents, setHasStudents] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const API_BASE_URL = window.location.origin;
+  
+  const fetchStudents = async () => {
+    try {
+      const response = await fetch(
+        `${API_BASE_URL}/api/students/read?school_id=${session?.user?.school_id}`
+      );
+      const data = await response.json();
+      setStudents(data);
+      setHasStudents(data.length > 0);
+    } catch (error) {
+      console.error("Error fetching students", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (session?.user?.school_id) {
+      fetchStudents();
+    }
+  }, [session?.user?.school_id]);
+
   return (
-    // <SidebarProvider>
-    // <AppSidebar />
-    // <SidebarInset>
+
     <div>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b bg-white/50 backdrop-blur-md">
           <div className="flex flex-1 items-center gap-2 px-3">
@@ -41,6 +67,15 @@ export default function Page() {
           </div>
         </header>
 
+        <div>
+        {loading ? (
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="animate-spin rounded-full h-6 w-6 border-2 border-blue-100 border-t-blue-600"></div>
+          </div>
+        ) : (
+          <>
+
+
         <div className="flex flex-col gap-4 p-4 overflow-x-hidden w-full">
           <div className="grid auto-rows-min gap-4 w-full">
             <div className="min-h-[60px] rounded-xl bg-blue-50 flex items-center px-4 w-full box-border overflow-hidden">
@@ -52,16 +87,21 @@ export default function Page() {
           </div>
         </div>
 
-        <div className="flex flex-1 flex-col gap-4 p-4">
+{students.length}
+
+
+        </>
+        )}
+      </div>
+        {/* <div className="flex flex-1 flex-col gap-4 p-4">
           <div className="grid auto-rows-min gap-4 md:grid-cols-3">
             <div className="aspect-video rounded-xl bg-blue-50" />
             <div className="aspect-video rounded-xl bg-blue-50" />
             <div className="aspect-video rounded-xl bg-blue-50" />
           </div>
           <div className="min-h-[100vh] flex-1 rounded-xl bg-blue-50 md:min-h-min" />
-        </div>
-         {/* </SidebarInset>
-       </SidebarProvider> */}
+        </div> */}
+       
       </div>
   )
 }
