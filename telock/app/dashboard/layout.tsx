1cdf2387225f7d3f8db4a96faa1a0aecfe186e96
szalkaiszaboadam <szalkai-szabo.adam@diak.szbi-pg.hu>
2025-04-02 10:59:29 +0200

    Update layout.tsx

--- telock/app/dashboard/layout.tsx
+++ telock/app/dashboard/layout.tsx
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth";
 import type { Metadata } from "next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth-options";
 import Providers from "../providers";
-import "./globals.css";
+import "../globals.css";
 
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
