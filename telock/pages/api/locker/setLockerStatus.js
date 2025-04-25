bbb9410fff29b12c18da3b64a1fc47c09a0051d6
nagygabor123 <nagy.gabor@diak.szbi-pg.hu>
2025-04-25 10:59:22 +0200

    Update setLockerStatus.js

--- telock/pages/api/locker/setLockerStatus.js
+++ telock/pages/api/locker/setLockerStatus.js
@@ -12,7 +12,6 @@ export default async function handler(req, res) {
     }
 
     try {
-
       const rows = await sql('SELECT status FROM lockers WHERE locker_id = $1', [lockerId]);
 
       if (rows.length === 0) {
@@ -41,21 +40,40 @@ export default async function handler(req, res) {
       }
 
       const rfidTag = relationshipRows[0].rfid_tag;
-      const studentUpdateCount = await sql(
-        'UPDATE students SET access = $1 WHERE rfid_tag = $2',
-        ['zarva', rfidTag]
+
+      const studentRows = await sql(
+        'SELECT access FROM students WHERE rfid_tag = $1',
+        [rfidTag]
       );
 
-      if (studentUpdateCount === 0) {
-        return res.status(500).json({ message: 'Sikertelen diák access frissítés' });
+      if (studentRows.length === 0) {
+        return res.status(404).json({ message: 'Diák nem található ezzel az RFID-val' });
       }
 
-      res.status(200).json({
-        message: `Szekrény ${lockerId} státusza frissítve '${newStatus}'-re, és a diák access mezője frissítve 'zarva' értékre.`,
-      });
+      const studentAccess = studentRows[0].access;
+
+      if (studentAccess === 'nyithato') {
+        const updateResult = await sql(
+          'UPDATE students SET access = $1 WHERE rfid_tag = $2',
+          ['zarva', rfidTag]
+        );
+
+        if (updateResult === 0) {
+          return res.status(500).json({ message: 'Sikertelen diák access frissítés' });
+        }
+
+        return res.status(200).json({
+          message: `Szekrény ${lockerId} státusza '${newStatus}', diák hozzáférés 'zarva'-ra állítva.`,
+        });
+      } else {
+        return res.status(200).json({
+          message: `Szekrény ${lockerId} státusza '${newStatus}', diák hozzáférése változatlan maradt (${studentAccess}).`,
+        });
+      }
+
     } catch (error) {
       console.error(error);
-      res.status(500).json({ message: 'Hiba az adatok feltöltésekor' });
+      res.status(500).json({ message: 'Hiba az adatok frissítésekor' });
     }
   } else {
     res.status(405).json({ message: 'A módszer nem engedélyezett' });
